030a41ab191207c95f7a473c3c4c54c11d0c463d
Carla Galarza <cmg2228@columbia.edu>
2017-06-08 14:30:55 -0400

    Adding edit page for user records. Displaying more information on index page about each user. Correctly redirecting to user index page after records are created/updated/deleted.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,45 +1,67 @@
 class Admin::UsersController < ApplicationController
-	load_and_authorize_resource
+  load_and_authorize_resource
 
-	def new
-		@user = User.new
-	end
+  def new
+    @user = User.new
+  end
 
-	def create
-		@user = User.new(user_params)
-		@user.provider = "saml"
-		if @user.save
-			flash[:success] = "User successfully added"
-			redirect_to admin_path
-		else
-			flash[:error] = @user.errors.full_messages.to_sentence
-			render :new
-		end
-	end
+  def edit
+    @user = User.find(params[:id])
+  end
 
-	def index
-		@users = User.all
-	end
+  def create
+    @user = User.new(user_params)
+    @user.provider = "saml"
+    if @user.save
+      flash[:success] = "User successfully added"
+      redirect_to admin_users_path
+    else
+      flash[:error] = @user.errors.full_messages.to_sentence
+      render :new
+    end
+  end
 
-	def destroy
-		@user = User.find(params[:id])
-		@user.destroy
-		render json: { message: "removed" }, status: :ok
-	end
+  def index
+    @users = User.all
+  end
 
-	def update
-		@user = User.find(params[:id])
-		if @user.update(user_params)
-			render json: { message: "updated" }, status: :ok
-		else
-			render json: { message: @user.errors.full_messages.to_sentence }, status: :internal_server_error
-		end
-	end
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    respond_to do |f|
+      f.html {
+        flash[:success] = "User successfully deleted"
+        redirect_to admin_users_path
+      }
+      f.json { render json: { message: "removed" }, status: :ok }
+    end
+  end
 
-	private
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      respond_to do |f|
+        f.html {
+          flash[:success] = "User successfully updated"
+          redirect_to admin_users_path
+        }
+        f.json { render json: { message: "updated" }, status: :ok }
+      end
+    else
+      error = @user.errors.full_messages.to_sentence
+      respond_to do |f|
+        f.html {
+          flash[:error] = error
+          render :edit
+        }
+        f.json { render json: { message:  error }, status: :internal_server_error }
+      end
+    end
+  end
 
-	def user_params
-		params.require(:user).permit(:email, :provider, :uid, :role)
-	end
+  private
 
+  def user_params
+    params.require(:user).permit(:email, :provider, :uid, :role)
+  end
 end
