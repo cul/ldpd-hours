ab25e689b1b96839bce97ddc3e3f5e8c0bec908b
Oneill38 <Megan.Oneill38@gmail.com>
2017-06-15 12:45:03 -0400

    rm sqlite gem

--- spec/features/time_tables_spec.rb
+++ spec/features/time_tables_spec.rb
@@ -1,25 +1,32 @@
 require 'rails_helper'
 
-describe "TimeTables" do
-  include_context 'login admin user'
-  
+describe "TimeTables", js: true do
+
   before(:each) do
     @library = Library.create(name: "Lehman", code: "lehman")
-    visit("/admin")
-    first("li a").click
   end
 
-  describe "batch edit page", js: true do
+  describe "viewing the page" do
+    include_context 'login admin user'
+
     it "should display a calendar" do
+      visit(time_tables_batch_edit_path(@library))
       expect(page).to have_css("table")
     end
+  end
+
+
+  describe "editing the calendar", js: true do 
+    include_context 'login admin user'
 
     it "should add dates to the sidebar when clicked" do
+      visit(time_tables_batch_edit_path(@library))
       first("tr td").click
       expect(page).to have_css('.days-list li', count: 1)
     end
 
     it "should save dates" do
+      visit(time_tables_batch_edit_path(@library))
       find("td", :text => "25").click
       select "07 AM", :from => "time_table_open_4i"
       select "30", :from => "time_table_open_5i"
@@ -30,6 +37,7 @@ describe "TimeTables" do
     end
 
     it "should not save dates with invalid hours" do
+      visit(time_tables_batch_edit_path(@library))
       find("td", :text => "25").click
       select "07 PM", :from => "time_table_open_4i"
       select "30", :from => "time_table_open_5i"
@@ -39,5 +47,6 @@ describe "TimeTables" do
       expect(find("div .alert-danger ul li").text).to eq("Please Enter Valid Data")
     end
   end
+
 
 end
