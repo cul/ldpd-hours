1312dd7aaa9aa703dd43586265f1cc24ca8f9d71
Carla Galarza <cmg2228@columbia.edu>
2017-06-29 15:41:26 -0400

    Adding timezone configuration to application.

--- config/application.rb
+++ config/application.rb
@@ -17,5 +17,7 @@ module LdpdHours
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.action_dispatch.rescue_responses.merge! 'CanCan::AccessDenied' => :forbidden
+
+    config.time_zone = 'Eastern Time (US & Canada)'
   end
 end
