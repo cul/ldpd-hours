2340243f506fce788ea49ab4ac8324d99336076d
Carla Galarza <cmg2228@columbia.edu>
2017-07-14 11:51:49 -0400

    Overriding devise/registrations routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {sessions: 'users/sessions', omniauth_callbacks: 'users/omniauth_callbacks'}
   root to: "locations#index"
-  
-  resources :users
 
+  resources :users
+  devise_for :users, controllers: {sessions: 'users/sessions', omniauth_callbacks: 'users/omniauth_callbacks'}
   devise_scope :user do
     get 'sign_in', to: 'users/sessions#new', as: :new_user_session
     get 'sign_out', to: 'users/sessions#destroy', as: :destroy_user_session
