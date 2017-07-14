6c5e279569ad3fe8d8927f406ce9dced4461b6a6
Carla Galarza <cmg2228@columbia.edu>
2017-07-14 11:21:22 -0400

    Changing authorization check on admin page. Can see admin page if they can atleast edit one location.

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,7 +1,8 @@
 class AdminsController < ApplicationController
-  skip_load_resource
+  skip_load_and_authorize_resource
 
   def show
+    authorize! :edit, Location
     @locations = Location.all
   end
 end
