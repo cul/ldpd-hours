048294a74c6dc6a20878c8c92fdb13db95748509
Benjamin Armintor <armintor@gmail.com>
2017-08-11 17:44:52 -0400

    use the developer omniauth strategy in development

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,5 +1,10 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   include Cul::Omniauth::Callbacks
-  skip_before_action :authenticate_user!
+  skip_before_action :authenticate_user!, :verify_authenticity_token
   skip_load_and_authorize_resource
+  def developer
+    current_user ||= User.find_or_create_by(email:request.env["omniauth.auth"][:uid])
+
+    sign_in_and_redirect current_user, event: :authentication
+  end
 end
