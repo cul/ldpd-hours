6f170d44cd404cdeb176d46ed53240cc81a3d8ce
Carla Galarza <cmg2228@columbia.edu>
2017-08-15 10:55:48 -0400

    Adding administrative user when seeding the database.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,7 +3,10 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   skip_before_action :authenticate_user!, :verify_authenticity_token
   skip_load_and_authorize_resource
   def developer
-    current_user ||= User.find_or_create_by(email:request.env["omniauth.auth"][:uid])
+    current_user ||= User.find_or_create_by(
+      email: request.env["omniauth.auth"][:uid], provider: :developer)
+
+    current_user.update(name: request.env["omniauth.auth"][:info][:name])
 
     sign_in_and_redirect current_user, event: :authentication
   end
