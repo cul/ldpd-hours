e191732ce6f26348cb8c675440663cd6112ebd33
Benjamin Armintor <armintor@gmail.com>
2017-08-24 18:17:49 -0400

    fold open_now display into home page

--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -2,4 +2,15 @@ module CalendarHelper
   def calendar_builder(date = Date.current, &block)
     CalendarBuilder.new(self, date, block).table
   end
+
+  def until_or_closed(location, open)
+    @closings ||= begin
+      closings = {}
+      open.each do |timetable|
+        closings[timetable.location_id] = "Until #{timetable.close_time.strftime("%l:%M %P")}"
+      end
+      closings
+    end
+    @closings.fetch(location.id, "CLOSED")
+  end
 end
---
+++ spec/helpers/calender_helper_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+describe CalendarHelper do
+  let(:now) { Time.current }
+  let(:today) { now.to_date }
+  let(:open) { now - 1.hour }
+  let(:still_open) { now + 1.hour }
+  let(:closed) { now - 30.minutes }
+  let(:subject_class) { s = Class.new { include CalendarHelper } }
+
+  context "no open libraries" do
+    subject { subject_class.new }
+    it do
+      expect(subject.until_or_closed(Location.new(id: 1), [])).to eql("CLOSED")
+    end
+  end
+
+  context "open library" do
+    subject { subject_class.new }
+    let(:timetable) { Timetable.new(location_id: 1, date: today, open: open, close: still_open) }
+    it do
+      expect(subject.until_or_closed(Location.new(id: 1), [timetable])).to match("Until")
+    end
+  end
+end
