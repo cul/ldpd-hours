692c78ac5f0e4589a2bf7014a00897f0687113e2
Eric O <eric@ejdigitalmedia.com>
2019-04-12 13:28:29 -0400

    Use 'public' layout for errors so that public viewers of the hours app don't see admin layout menu items. Note: Error handling views need to be re-evaluated in the future. This is a quick fix.

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,6 +1,8 @@
 class ErrorsController < ApplicationController
   include Gaffe::Errors
 
+  layout 'public'
+
   # Make sure anonymous users can see the page
   skip_before_action :authenticate_user!
   skip_load_and_authorize_resource
