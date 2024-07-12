0d4cee5c8a5ea0bb90ace66ea9c5bad05f50449c
Benjamin Armintor <armintor@gmail.com>
2024-07-12 11:52:01 -0400

    bootstrap classes on location edit form for readability (HOURS-128)

--- app/helpers/edit_location_helper.rb
+++ app/helpers/edit_location_helper.rb
@@ -3,11 +3,15 @@ module EditLocationHelper
   def access_point_checkboxes(access_point_branches, count_iterator, current_values = [])
     content_tag(:ul) do
       access_point_branches.map do |branch|
-        branch_tag = content_tag(:li) do
+        branch_tag = content_tag(:li, class: ['form-check']) do
           cb_index = count_iterator.next
-          content_tag(:label, for: "location_access_points_#{cb_index}") do
+          content_tag(:label, for: "location_access_points_#{cb_index}", class: ['form-check-label']) do
             checked = current_values.include?(branch.id)
-            tag(:input, type: 'checkbox', value: branch.id, name: "location[access_points][]", id: "location_access_points_#{cb_index}", checked: checked).safe_concat("#{branch.name} #{branch.updated_at}")
+            cb_attrs = {
+              type: 'checkbox', value: branch.id, name: "location[access_points][]",
+              id: "location_access_points_#{cb_index}", checked: checked, class: ['form-check-input']
+            }
+            tag(:input, cb_attrs).safe_concat("#{branch.name}").safe_concat(content_tag(:em, branch.updated_at ? "updated: #{branch.updated_at}" : "aggregate data", class: "ml-1"))
           end
         end
         if branch.children.size > 0
