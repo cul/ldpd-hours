7dc9c0a1e64df5609cea8d3acc7af9a416c7929c
Benjamin Armintor <armintor@gmail.com>
2025-10-27 12:27:41 -0400

    do not assign User attributes from ldap unless necessary; spec ldap assignment

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,10 +2,12 @@ FactoryBot.define do
   factory :admin, class: User do
     uid { 'abc123' }
     email { 'abc123@columbia.edu' }
+    name { 'Abc 123' }
   end
 
   factory :user, class: User do
     uid { 'xyz123'}
     email { 'xyz123@columbia.edu' }
+    name { 'Xyz 123' }
   end
 end
