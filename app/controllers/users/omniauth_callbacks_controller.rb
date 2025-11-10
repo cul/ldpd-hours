59a490ede4e6dfc4f9d386f5e7f3a4f7da220e2c
Weronika Tomaszewska <wt2392@columbia.edu>
2025-11-10 17:36:21 -0500

    HOURS-143 add authenticate_user! to skip_before_action, add logging

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,7 +3,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # The CAS login redirect to the columbia_cas callback endpoint AND the developer form submission to the
   # developer_uid callback do not send authenticity tokens, so we'll skip token verification for these actions.
   skip_before_action :verify_authenticity_token, only: [:columbia_cas, :developer_uid]
-  # skip_before_action :authenticate_user!, :verify_authenticity_token
+  skip_before_action :authenticate_user!
   skip_load_and_authorize_resource
 
   # POST /users/auth/developer_uid/callback
@@ -25,19 +25,29 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def columbia_cas
+    Rails.logger.info "Columbia CAS callback started"
+    Rails.logger.info "Request params: #{request.params.inspect}"
+    
     callback_url = user_columbia_cas_omniauth_callback_url
     uid, _affils = Omniauth::Cul::ColumbiaCas.validation_callback(request.params['ticket'], callback_url)
-    puts "Received user_id: #{uid}, affils: #{_affils.inspect}"
+    Rails.logger.info "Received user_id: #{uid}, affils: #{_affils.inspect}"
 
     current_user = User.find_by(uid: uid)
 
     if !current_user
+      Rails.logger.error "User not found for uid: #{uid}"
       flash[:alert] = "Login attempt failed.  Please try again."
       redirect_to root_path
       return
     end
 
+    Rails.logger.info "Found user: #{current_user.uid}, signing in..."
     sign_in_and_redirect current_user, event: :authentication
+  rescue => e
+    Rails.logger.error "Columbia CAS callback error: #{e.message}"
+    flash[:alert] = "Authentication failed. Please try again."
+    redirect_to root_path
+  end
   
     # Custom auth logic for your app goes here.
     # The code below is provided as an example.  If you want to use Omniauth::Cul::PermissionFileValidator,
@@ -53,7 +63,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     #   flash[:error] = 'Login attempt failed'
     #   redirect_to root_path
     # end
-  end
 
   # GET /users/auth/cas/callback
   # def cas
